38cc6e1babdda7122091df43fca82cb52bf42618
tatiana_sht <tatianaorlova0271@gmail.com>
2023-06-13 15:47:25 +1000

    Added solution

---
+++ Exercise002_ThirdSymbol/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+// что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 99 && number < 1000)
+{
+    Console.Write($"В этом числе третья цифра - ");
+    Console.WriteLine(number % 10);
+}
+else if (number < 100)
+{
+    Console.Write($"В этом числе третьей цифры нет");
+}
+
+else if (number > 999)
+{
+    while (number > 999)
+    {
+        number = number / 10;
+    }
+    number = number % 10;
+    Console.Write($"В этом числе третья цифра - ");
+    Console.WriteLine(number);
+}
